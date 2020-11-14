7eb2a834036ba1fbfb52ece1189a9d325e5d53ef
gangulanikhilreddy <74185262+gangulanikhilreddy@users.noreply.github.com>
2020-11-15 00:14:04 +0530

    added venue

--- ni.js
+++ ni.js
@@ -1,4 +1,5 @@
-var kkr = require("././-MS_Faker_Lynx_Tep_Online_Full-time_2020_3")
+var kkr = require("../fkr")
 console.log("Ipl Team : ", kkr.ipl.team());
 console.log("Captain : ", kkr.ipl.captain());
 console.log("Won Cup In Year : ", kkr.ipl.year());
+console.log("stadium : ", kkr.ipl.stadium());
