3e51d8ddd783bf0448f2d3987fd10b42382479fb
gangulanikhilreddy <74185262+gangulanikhilreddy@users.noreply.github.com>
2020-11-15 00:58:18 +0530

    added pull request

--- ni.js
+++ ni.js
@@ -3,3 +3,4 @@ console.log("Ipl Team : ", kkr.ipl.team());
 console.log("Captain : ", kkr.ipl.captain());
 console.log("Won Cup In Year : ", kkr.ipl.year());
 console.log("stadium : ", kkr.ipl.stadium());
+console.log("Franchise Owner : ", kkr.franchise.owner());
